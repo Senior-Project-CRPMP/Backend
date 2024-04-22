56906448090875e2d0d5b7735f17af6358ed5efd
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2024-04-22 13:31:56 +0300

    weather controller

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc7f473402d17aa7fae43c069080dc02d37a61b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e20f8163010d2e45710a0bf5efe16f844a0783d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
